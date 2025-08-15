49fec4b2e3c76e64323c28e1f07366e16740dfcc
Макаренко Анна Николаевна <makarenko-an@part-kom.ru>
2025-08-15 13:41:23 +0300

    feat: add basic avatar flow

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-const myStorageKey = 'nfcCharacterName';
+const myNameStorageKey = 'nfcCharacterName';
+const myImageStorageKey = 'nfcCharacterImageID';
 const windowsNames = ["Main", "Character", "Settings", "Battle"];
 
 let headerSpan, firstWindow, secondWindow, formContainers;
 let currentNameSpan, newNameInput, editNameButton, saveNameButton;
+let characterAvatar;
+
+function randomInteger(min, max) {
+    let rand = min + Math.random() * (max - min);
+    return Math.floor(rand);
+}
 
 function createNewCharacter() {
     const characterNameInput = document.getElementById('character-name');
     setCurrentCharacterName(characterNameInput.value);
+
+    const randomAvatarID = randomInteger(1, 8);
+    setCurrentCharacterImage(randomAvatarID);
+
+    switchWindows();
 }
 
 function switchWindows() {
@@ -34,17 +46,30 @@ function switchToContainer(idx) {
 }
 
 function setCurrentCharacterName(newName) {
-    localStorage.setItem(myStorageKey, newName);
+    localStorage.setItem(myNameStorageKey, newName);
+}
+
+function setCurrentCharacterImage(imageID) {
+    localStorage.setItem(myImageStorageKey, imageID);
 }
 
 function getCurrentCharacterName() {
-    return localStorage.getItem(myStorageKey);
+    return localStorage.getItem(myNameStorageKey);
+}
+
+function getCurrentCharacterImage() {
+    return localStorage.getItem(myImageStorageKey);
 }
 
 function setNameInSettingsContainer() {
     const currentName = getCurrentCharacterName();
     currentNameSpan.textContent = currentName;
     newNameInput.value = currentName;
+}
+
+function setNameInCharacterContainer() {
+    const nameElement = document.querySelector('.my-character-name');
+    nameElement.textContent = getCurrentCharacterName();
 }
 
 function switchSettingsElements() {
@@ -54,12 +79,43 @@ function switchSettingsElements() {
     saveNameButton.classList.toggle('hidden-element');
 }
 
+function drawCharacterPicture(idx) {
+    characterAvatar.classList.forEach(item => {
+        if (item !== 'character-image-container') {
+            characterAvatar.classList.remove(item);
+        }
+    });
+    characterAvatar.classList.add('character-' + idx);
+}
+
+function handleCharacterIconClick() {
+    setNameInCharacterContainer();
+
+    const currentPictureID = getCurrentCharacterImage();
+    drawCharacterPicture(currentPictureID);
+
+    switchToContainer(1);
+}
+
+function handleSettingsIconClick() {
+    setNameInSettingsContainer();
+    switchToContainer(2);
+}
+
+function handleNameEdit() {
+    setCurrentCharacterName(newNameInput.value);
+    setNameInSettingsContainer();
+    switchSettingsElements();
+}
+
 function getWindowElements() {
     firstWindow = document.querySelector('.create-character');
     secondWindow = document.querySelector('.game-flow-container');
 
     headerSpan = document.querySelector(".window-name-span");
     formContainers = document.querySelectorAll('.my-containers');
+
+    characterAvatar = document.querySelector('.character-image-container');
 
     currentNameSpan = document.querySelector('.current-name');
     newNameInput = document.getElementById('new-character-name');
@@ -71,24 +127,14 @@ document.addEventListener('DOMContentLoaded', () => {
     getWindowElements();
 
     const createCharacterButton = document.querySelector('.create-character-button');
-    createCharacterButton.addEventListener('click', () => {
-        createNewCharacter();
-        switchWindows();
-    });
+    createCharacterButton.addEventListener('click', createNewCharacter);
 
     document.querySelector('.home-icon').addEventListener('click', () => switchToContainer(0));
-    document.querySelector('.character-icon').addEventListener('click', () => switchToContainer(1));
+    document.querySelector('.character-icon').addEventListener('click', handleCharacterIconClick);
 
-    document.querySelector('.settings-icon').addEventListener('click', () => {
-        setNameInSettingsContainer();
-        switchToContainer(2);
-    });
-    editNameButton.addEventListener('click', () => switchSettingsElements());
-    saveNameButton.addEventListener('click', () => {
-        setCurrentCharacterName(newNameInput.value);
-        setNameInSettingsContainer();
-        switchSettingsElements();
-    });
+    document.querySelector('.settings-icon').addEventListener('click', handleSettingsIconClick);
+    editNameButton.addEventListener('click', switchSettingsElements);
+    saveNameButton.addEventListener('click', handleNameEdit);
 
     document.querySelector('.fight-button').addEventListener('click', () => switchToContainer(3));
 
